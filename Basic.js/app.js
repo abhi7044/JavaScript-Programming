4cda9427f057a1dc51047d0741375c2ba58151be
Abhijeet Mishra <teslanikola664@gmail.com>
2026-06-08 21:04:08 +0530

    Add: Basic.ja - first JavaScript program

---
+++ Basic.js/app.js
@@ -0,0 +1,35 @@
+// Lecture - 2
+console.log("Hello world");
+console.log("Apna College"); 
+let a = 10;
+let b = 5;
+console.log("Sum is: ", a + b);
+
+// Lecture - 3
+let pencilprice = 10 ;
+let erasorprice = 5;
+let output = `The total price is : ${pencilprice} Rupees.`;
+console.log(output);
+
+// Lecture - 4
+let x = 10;
+let y = 5;
+console.log(a + b);
+console.log(a - b);
+console.log(a * b);
+console.log(a / b);
+console.log(a % b);
+console.log(a ** b);
+console.log(a++);
+console.log(++a);
+console.log(++a);
+y = x;
+console.log(y);
+
+// Lecture - 5
+let age = 23;
+console.log(age>18);
+let num = 5; 
+let str = '5';
+console.log(num == str); // return true;
+console.log(num === str); // return false;
