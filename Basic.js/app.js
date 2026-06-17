528ecce015d885a7b5a7a201b51f662d47f1f954
Abhijeet Mishra <teslanikola664@gmail.com>
2026-06-17 21:34:12 +0530

    Update: Basic.js - added the switch case statement

--- Basic.js/app.js
+++ Basic.js/app.js
@@ -161,3 +161,19 @@ if(true){
 }else{
     console.log("it has false value");
 }
+
+// lecture 17 ---> Switch case statement 
+let colorrr = "red";
+switch (colorrr) {
+    case "red":
+        console.log("Stop");
+        break;
+    case "yellow":
+        console.log("Slow down");
+        break;
+    case "green":
+        console.log("Go");
+        break;
+    default:
+        console.log("Broken light ");
+    } 
