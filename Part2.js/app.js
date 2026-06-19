5491525ede0929a2d269eb040e160919d508e797
Abhijeet Mishra <teslanikola664@gmail.com>
2026-06-19 19:58:38 +0530

    Update: Part2.js - added the trim method , toUpperCase & toLowerCase and in this also known String are immutable

---
+++ Part2.js/app.js
@@ -0,0 +1,16 @@
+// Trim Method  
+let msg = "          hello               ";
+msg = msg.trim();
+console.log(msg);
+
+let password = prompt("set your password");
+let newpass = password.trim();
+console.log(newpass);
+
+// toUpperCase() and toLowerCase()
+let name = "Apna College";
+console.log(name.toUpperCase());
+
+let mssg = "errror";
+console.log(mssg.toUpperCase());
+
