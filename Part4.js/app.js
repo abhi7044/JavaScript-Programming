21981f829af9eac5b350e092c19a95c2db129809
Abhijeet Mishra <teslanikola664@gmail.com>
2026-08-09 00:56:17 +0530

    Update: Part4.js for Loop

---
+++ Part4.js/app.js
@@ -0,0 +1,40 @@
+// For loop 
+console.log("1");
+console.log("2");
+console.log("3");
+console.log("4");
+console.log("5");
+
+for(let i = 1; i <= 5; i++) {
+    console.log(i);
+}
+
+for(let i = 5; i >= 1; i--) {
+    console.log(i);
+}
+
+for(let i = 10; i >= 1; i = i-3) {
+    console.log(i);
+}
+
+// Print all the odd numbers from 1 to 15
+for(let i =1; i <=15; i = i+2){
+   console.log(i);
+}
+
+// print all the even numbers from 2 to 10
+for(let i =2; i <=10; i = i+2) {
+    console.log(i);
+}
+
+// Infinite loop
+// for(let i = 1; i >= 0; i++) {
+//     console.log(i);
+// }
+// for(let i = 1; i <= 5; i--) {
+//     console.log(i);
+// }
+// for(let i =1; ; i++){
+//     console.log(i);
+// }
+
