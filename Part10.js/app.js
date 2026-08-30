f06e8dfe80c12ac471307f59f32329abddce28d3
Abhijeet Mishra <teslanikola664@gmail.com>
2026-08-30 13:20:26 +0530

    Add : Onclick and OnmouseEnter property

---
+++ Part10.js/app.js
@@ -0,0 +1,36 @@
+let btn = document.querySelectorAll("button");
+// for (btns of btn) {
+//     btns.onclick = sayHello;
+//     btns.onmouseenter = function () {
+//         console.log("you entered a button");
+//     };
+//     console.dir(btn);
+// }
+
+// btn.onclick = function () {
+    // console.log("button was clicked");
+//     alert("button was clicked");
+// };
+
+// function sayHello(){
+//     alert("Hello");
+// }
+
+
+// function sayName(){
+//     alert("apna college");
+// }
+
+// btn.onclick = sayHello;
+
+// EventListener ?
+// for (btns of btn) {
+    // btns.addEventListener("click", sayHello);
+    // btns.addEventListener("click", sayName);
+//     btns.addEventListener("dblclick", function (){
+//         alert("you double clicked me");
+//     });
+
+// }
+ 
+// Activity
